adbcad1dd81014e7c15eaf3545b4dcf3a822e4ac
lemuellloren <lempakz@yahoo.com>
2022-04-12 10:27:01 +0800

    build nodejs express app

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const graphqlHTTP = require('express-graphql')
-const graphql = require('graphql')
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const graphql = require('graphql');
 
 const QueryRoot = new graphql.GraphQLInputObjectType({
     name: 'Query',
@@ -10,7 +10,14 @@ const QueryRoot = new graphql.GraphQLInputObjectType({
             resolve: () => 'Hello World!'
         }
     }) 
-})
+});
 
 const schema = new graphql.GraphQLSchema({ query: QueryRoot })
 
+const app = express();
+const port = 4000;
+app.use('/api', graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+}));
+app.listen(port);
