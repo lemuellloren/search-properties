5d498f9a28180b5e747e9d4e640118acd101e58f
lemuellloren <lempakz@yahoo.com>
2022-04-12 09:46:33 +0800

    create first schema and resolver

--- server/server.js
+++ server/server.js
@@ -1,3 +1,13 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const graphql = require('graphql')
+
+const QueryRoot = new graphql.GraphQLInputObjectType({
+    name: 'Query',
+    fields: () => ({
+        hello: {
+            type: graphql.GraphQLString,
+            resolve: () => 'Hello World!'
+        }
+    }) 
+})
