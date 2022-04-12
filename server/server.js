392bb0b6c576a91fb7cbd3db68e4560be2f1136c
lemuellloren <lempakz@yahoo.com>
2022-04-12 10:38:09 +0800

    fix graphql http error

--- server/server.js
+++ server/server.js
@@ -1,21 +1,22 @@
 const express = require('express');
-const graphqlHTTP = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql');
 const graphql = require('graphql');
 
-const QueryRoot = new graphql.GraphQLInputObjectType({
+const QueryRoot = new graphql.GraphQLObjectType({
     name: 'Query',
     fields: () => ({
-        hello: {
-            type: graphql.GraphQLString,
-            resolve: () => 'Hello World!'
-        }
-    }) 
-});
+      hello: {
+        type: graphql.GraphQLString,
+        resolve: () => "Hello world!"
+      }
+    })
+  })
 
-const schema = new graphql.GraphQLSchema({ query: QueryRoot })
+const schema = new graphql.GraphQLSchema({ query: QueryRoot });
+
 
 const app = express();
-const port = 4000;
+const port = 8000;
 app.use('/api', graphqlHTTP({
     schema: schema,
     graphiql: true,
