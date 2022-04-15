dc6e71cb39ec145e06c64add5e85f22d22c8369b
lemuellloren <lempakz@yahoo.com>
2022-04-15 14:41:28 +0800

    refactor server.js

--- server/server.js
+++ server/server.js
@@ -1,18 +1,90 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const graphql = require('graphql');
+const joinMonster = require('join-monster');
+
+// connect database 
+const { Client } = require('pg')
+const client = new Client({
+  host: "localhost",
+  user: "lemuellloren",
+  password: "",
+  database: "properties"
+})
+client.connect()
+
+// define schema 
+const Property = new graphql.GraphQLObjectType({
+  name: 'Property',
+  fields: () => ({
+    id: { type: graphql.GraphQLString },
+    street: { type: graphql.GraphQLString },
+    city: { type: graphql.GraphQLString },
+    state: { type: graphql.GraphQLString },
+    postal_code: { type: graphql.GraphQLString },
+    rent: { type: graphql.GraphQLInt },
+    owner: {
+      sqlJoin: (propertyTable, ownerTable, args) => `${propertyTable}.property_id = ${ownerTable}.id`
+    }
+  })
+});
+
+Property_typeConfig = {
+  sqlTable: 'property',
+  uniqueKey: 'id',
+}
+
+let Owner = new graphql.GraphQLObjectType({
+  name: 'Owner',
+  fields: () => ({
+    id: { type: graphql.GraphQLString },
+    first_name: { type: graphql.GraphQLString },
+    last_name: { type: graphql.GraphQLString },
+    properties: {
+      type: graphql.GraphQLList(Property),
+      sqlJoin: (ownerTable, propertyTable, args) => `${ownerTable}.id = ${propertyTable}.property_id`
+   }
+  })
+})
+    
+Owner._typeConfig = {
+  sqlTable: 'owner',
+  uniqueKey: 'id'
+}
+
 
 const QueryRoot = new graphql.GraphQLObjectType({
-    name: 'Query',
-    fields: () => ({
-      hello: {
-        type: graphql.GraphQLString,
-        resolve: () => "Hello world!"
+  name: 'Query',
+  fields: () => ({
+    hello: {
+      type: graphql.GraphQLString,
+      resolve: () => "Hello world!"
+    },
+    properties: {
+      type: new graphql.GraphQLList(Property),
+      resolve: (parent, args, context, resolveInfo) => {
+        return joinMonster.default(resolveInfo, {}, sql => {
+          return client.query(sql)
+        })
       }
-    })
+    },
+    property: {
+      type: Property,
+      args: { id: { type: graphql.GraphQLNonNull(graphql.GraphQLInt) } },
+      where: (propertyTable, args, context) => `${propertyTable}.id = ${args.id}`,
+      resolve: (parent, args, context, resolveInfo) => {
+        return joinMonster.default(resolveInfo, {}, sql => {
+          return client.query(sql)
+        })
+     }
+    }
   })
+});
 
-const schema = new graphql.GraphQLSchema({ query: QueryRoot });
+
+const schema = new graphql.GraphQLSchema({
+  query: QueryRoot
+});
 
 
 const app = express();
@@ -23,44 +95,11 @@ app.use('/api', graphqlHTTP({
 }));
 app.listen(port);
 
-const { Client } = require('pg')
-const client = new Client({
-  host: "localhost",
-  user: "lemuellloren",
-  password: "",
-  database: "properties"
-})
-client.connect()
 
-const joinMonster = require('join-monster');
 
-const Owner = new graphql.GraphQLObjectType({
-  name: 'Owner',
-  fields: () => ({
-    id: { type: graphql.GraphQLString },
-    first_name: { type: graphql.GraphQLString },
-    last_name: { type: graphql.GraphQLString }
-  })
-});
 
-Owner._typeConfig = {
-  sqlTable: 'owner',
-  uniqueKey: 'id',
-}
 
-const Property = new graphql.GraphQLObjectType({
-  name: 'Property',
-  fields: () => ({
-    property_id: { type: graphql.GraphQLString },
-    street: { type: graphql.GraphQLString },
-    city: { type: graphql.GraphQLString },
-    state: { type: graphql.GraphQLString },
-    postal_code: { type: graphql.GraphQLString },
-    rent: { type: graphql.GraphQLInt }
-  })
-});
 
-Property._typeConfig = {
-  sqlTable: 'property',
-  uniqueKey: 'property_id'
-}
+
+
+
