0384af9888e5f1c8a02fef45f763979f1f7357b1
lemuellloren <lempakz@yahoo.com>
2022-04-15 18:10:15 +0800

    add client apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
 
 function App() {
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: 'https://countries.trevorblades.com/'
+  })
   return (
-    <div className="App">
-        Hello World!
-    </div>
+    <ApolloProvider>
+        <div className="App">
+            Hello World!
+        </div>
+    </ApolloProvider>
+ 
   );
 }
 
